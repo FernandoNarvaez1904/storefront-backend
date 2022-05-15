c0fd1c366b2739f62fa437a5dd34114d38fc23da
diegoromero <yolos12345>
2022-05-15 14:33:05 -0600

    Created Custom User Model
    The Model is created at this stage, in other to allow extension in the future

--- storefront_backend/settings.py
+++ storefront_backend/settings.py
@@ -15,7 +15,6 @@ DEBUG = config("DEBUG") == "DEBUG"
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -25,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'storefront_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -67,7 +67,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -87,7 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -99,7 +97,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -109,3 +106,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = "users.User"
