c4346aef39b099e6b35c780fa9de5e95d151e770
diegoromero <yolos12345>
2022-05-21 12:24:15 -0600

    Make ProductDetail the current in Product
    Using post_save signals. Test Missing, I committed without the test because of failure in my IDE

--- inventory/models/product_model.py
+++ inventory/models/product_model.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Product(models.Model):
@@ -15,3 +17,11 @@ class ProductDetail(models.Model):
     cost = models.FloatField()
     markup = models.FloatField()
     root_product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_versions")
+
+
+# TODO TEST
+@receiver(post_save, sender=ProductDetail)
+def make_new_detail_current_detail(sender, instance: ProductDetail, **kwargs):
+    root_product = instance.root_product
+    root_product.current_detail = instance
+    root_product.save()
