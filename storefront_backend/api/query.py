d72814c82fae1c09ea354700a7bdced5a0109e0d
diegoromero <yolos12345>
2022-05-23 00:34:05 -0600

    Added Inventory queries to root app

--- storefront_backend/api/query.py
+++ storefront_backend/api/query.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
 from strawberry_django_plus import gql
+from inventory.api.query import Query as InventoryQuery
 
 
 @gql.type
-class Query:
+class Query(InventoryQuery):
     node: Optional[gql.Node] = gql.django.node()
