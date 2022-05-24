b468dca07fc06d47247ae540950ac09d2b66af0f
diegoromero <yolos12345>
2022-05-24 15:41:39 -0600

    Default returns when Product has no ProductType

--- inventory/api/types/product_type.py
+++ inventory/api/types/product_type.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from strawberry_django_plus import gql
 
@@ -15,27 +15,42 @@ class ProductType(gql.Node, ABC):
     is_active: gql.auto
 
     @gql.field
-    def name(self: Product) -> str:
-        return self.current_detail.name
+    def name(self: Product) -> Optional[str]:
+        try:
+            return self.current_detail.name
+        except AttributeError:
+            return None
 
     @gql.field
-    def barcode(self: Product) -> str:
-        return self.current_detail.barcode
+    def barcode(self: Product) -> Optional[str]:
+        try:
+            return self.current_detail.barcode
+        except AttributeError:
+            return None
 
     @gql.field
     def cost(self: Product) -> float:
-        return self.current_detail.cost
+        try:
+            return self.current_detail.cost
+        except AttributeError:
+            return 0
 
     @gql.field
     def markup(self: Product) -> float:
-        return self.current_detail.markup
+        try:
+            return self.current_detail.markup
+        except AttributeError:
+            return 0
 
     @gql.field
-    def last_modified_date(self: Product) -> datetime:
-        return self.current_detail.date
+    def last_modified_date(self: Product) -> Optional[datetime]:
+        try:
+            return self.current_detail.date
+        except AttributeError:
+            return None
 
     @gql.field
-    def current_stock(self: Product) -> float:
+    def current_stock(self: Product) -> Optional[float]:
         stock_changes: List[ModifyStockDocument] = self.stock_changes.all()
         stock = 0
         for i in stock_changes:
