410620282ba4cba6e54b751def7141ae8ea9f193
diegoromero <yolos12345>
2022-06-02 16:34:57 -0600

    Created ItemUpdateInput

---
+++ inventory/api/types/item/inputs/item_update_input.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from asgiref.sync import sync_to_async
+from strawberry_django_plus import gql
+
+from inventory.api.types.item import ItemIsNotActiveError, ItemNotExistError
+from inventory.models import Item, ItemDetail
+from storefront_backend.api.types import UserError
+
+
+@gql.django.input(ItemDetail)
+class ItemUpdateDataInput:
+    barcode: gql.auto
+    name: gql.auto
+    sku: gql.auto
+    cost: gql.auto
+    markup: gql.auto
+
+
+@gql.django.input(Item)
+class ItemUpdateInput:
+    id: gql.relay.GlobalID = gql.field(description="The id given must be of an existing and active Item.")
+    data: ItemUpdateDataInput
+
+    async def validate_and_get_errors(self) -> List[UserError]:
+        errors = []
+        item_list = await sync_to_async(list)(Item.objects.filter(id=self.id.node_id))
+
+        if item_list:
+            # As the filter was using id the resulting list will only have one result
+            item = item_list[0]
+
+            if not item.is_active:
+                errors.append(ItemIsNotActiveError(
+                    message=f"Item with id {self.id} is not active. Cannot deactivate inactive items"
+                ))
+        else:
+            errors.append(
+                ItemNotExistError(message=f"item with id {self.id} does not exist in database")
+            )
+            
+        return errors
