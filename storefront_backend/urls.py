2e687a683ab32d358693d869466787677f99153e
diegoromero <yolos12345>
2022-06-06 23:29:53 -0600

    QUitando comentario de prueba

--- storefront_backend/urls.py
+++ storefront_backend/urls.py
@@ -3,7 +3,6 @@ from strawberry.django.views import AsyncGraphQLView
 
 from storefront_backend.api.schema import schema
 
-# Probando Clickup
 urlpatterns = [
     path('graphql', AsyncGraphQLView.as_view(schema=schema)),
 ]
