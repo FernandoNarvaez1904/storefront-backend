b1469667135724cf66005709a23f99c8641aafde
diegoromero <yolos12345>
2022-06-14 18:38:42 -0600

    Importing ItemVersionType in the module

--- inventory/api/types/item/__init__.py
+++ inventory/api/types/item/__init__.py
@@ -1,4 +1,4 @@
-from .item_type import ItemType
+from .item_type import ItemType, ItemVersionType
 from .user_error_types import SKUNotUniqueError, BarcodeNotUniqueError, ItemNotExistError, ItemIsNotActiveError, \
     ItemIsActiveError, ItemAlreadyHasDocument
 
