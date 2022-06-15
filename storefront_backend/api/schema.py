fe3e083889d91a643ec3886113dee6b336f85d1d
diegoromero <yolos12345>
2022-06-15 02:37:30 -0600

    Optimizer Extension

--- storefront_backend/api/schema.py
+++ storefront_backend/api/schema.py
@@ -1,7 +1,9 @@
 from strawberry import Schema
+from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 
 from inventory.api.types.item import not_in_schema_types as inventory_not_in_schema_types
 from storefront_backend.api.mutation import Mutation
 from storefront_backend.api.query import Query
 
-schema = Schema(query=Query, mutation=Mutation, types=[*inventory_not_in_schema_types])
+schema = Schema(query=Query, mutation=Mutation, types=[*inventory_not_in_schema_types],
+                extensions=[DjangoOptimizerExtension])
