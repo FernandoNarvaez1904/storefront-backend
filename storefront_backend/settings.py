51a5e64c3127efa7d90fdb33b1316c92ee0a9d1c
diegoromero <yolos12345>
2022-06-15 00:40:41 -0600

    Default SQLite if not DATABASE_URL

--- storefront_backend/settings.py
+++ storefront_backend/settings.py
@@ -66,7 +66,15 @@ WSGI_APPLICATION = 'storefront_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
+try:
+    DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
+except:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
