87a13a88b4d0405a735c2e6fdfae3ce4034844f0
diegoromero <yolos12345>
2022-07-27 11:08:54 -0600

    Added Cache to Parsing and Validation

--- storefront_backend/api/schema.py
+++ storefront_backend/api/schema.py
@@ -1,4 +1,5 @@
 from strawberry import Schema
+from strawberry.extensions import ParserCache, ValidationCache
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 
 from inventory.api.types.item import not_in_schema_types as inventory_not_in_schema_types
@@ -6,4 +7,4 @@ from storefront_backend.api.mutation import Mutation
 from storefront_backend.api.query import Query
 
 schema = Schema(query=Query, mutation=Mutation, types=[*inventory_not_in_schema_types],
-                extensions=[DjangoOptimizerExtension])
+                extensions=[ParserCache(), ValidationCache(), DjangoOptimizerExtension()])
