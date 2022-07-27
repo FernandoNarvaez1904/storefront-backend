ea82c5e5968c3bfc1b1a146b2dac71c76e1e0f1b
diegoromero <yolos12345>
2022-07-27 16:29:11 -0600

    Making ItemType async

--- inventory/api/types/item/item_type.py
+++ inventory/api/types/item/item_type.py
@@ -2,6 +2,7 @@ from abc import ABC
 from datetime import datetime
 from typing import Optional
 
+from asgiref.sync import sync_to_async
 from strawberry import auto
 from strawberry_django_plus import gql
 from strawberry_django_plus.relay import GlobalID
@@ -28,51 +29,65 @@ class ItemType(gql.relay.Node, ABC):
     item_versions: gql.relay.Connection["ItemVersionType"] = gql.relay.connection()
 
     @gql.django.field
-    def name(self: Item) -> Optional[str]:
+    async def name(self: Item) -> Optional[str]:
         try:
-            return self.current_detail.name
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            return detail.name
         except AttributeError:
             return None
 
     @gql.django.field
-    def barcode(self: Item) -> Optional[str]:
+    async def barcode(self: Item) -> Optional[str]:
         try:
-            return self.current_detail.barcode
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            return detail.barcode
         except AttributeError:
             return None
 
     @gql.django.field
-    def cost(self: Item) -> float:
+    async def cost(self: Item) -> float:
         try:
-            return self.current_detail.cost
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            return detail.cost
         except AttributeError:
             return 0
 
     @gql.django.field
-    def markup(self: Item) -> float:
+    async def markup(self: Item) -> float:
         try:
-            return self.current_detail.markup
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            return detail.markup
         except AttributeError:
             return 0
 
     @gql.django.field
-    def creation_date(self: Item) -> Optional[datetime]:
+    async def creation_date(self: Item) -> Optional[datetime]:
         try:
-            return self.current_detail.date
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            return detail.date
         except AttributeError:
             return None
 
     @gql.django.field
-    def price(self: Item) -> Optional[float]:
+    async def price(self: Item) -> Optional[float]:
         try:
-            cost = self.current_detail.cost
-            return cost + (cost * (self.current_detail.markup / 100))
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            cost = detail.cost
+            return cost + (cost * (detail.markup / 100))
         except AttributeError:
             return None
 
     @gql.django.field
-    def version_id(self: Item) -> Optional[GlobalID]:
+    async def version_id(self: Item) -> Optional[GlobalID]:
         try:
-            return GlobalID(node_id=f"{self.current_detail.id}", type_name="ItemVersionType")
+            pk = self.current_detail_id
+            detail: ItemDetail = await sync_to_async(ItemDetail.objects.get)(pk=pk)
+            return GlobalID(node_id=f"{detail.id}", type_name="ItemVersionType")
         except AttributeError:
             return None
--- inventory/test/api/types/item/test_item_type.py
+++ inventory/test/api/types/item/test_item_type.py
@@ -1,4 +1,4 @@
-from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync, sync_to_async
 from django.test import TestCase
 from strawberry_django_plus.relay import GlobalID
 
@@ -13,43 +13,38 @@ class ItemTypeTest(TestCase):
         items = async_to_sync(create_bulk_of_item)(1)
         self.item = items[0]
 
-    def test_name_field(self):
-        name = ItemType.name(self.item)
+    async def test_name_field(self):
+        name = await ItemType.name(self.item)
         self.assertEqual(self.item.current_detail.name, name)
 
-    def test_barcode_field(self):
-        barcode = ItemType.barcode(self.item)
+    async def test_barcode_field(self):
+        barcode = await ItemType.barcode(self.item)
         self.assertEqual(self.item.current_detail.barcode, barcode)
 
-    def test_cost_gql(self):
-        cost = ItemType.cost(self.item)
+    async def test_cost_gql(self):
+        cost = await ItemType.cost(self.item)
         self.assertEqual(self.item.current_detail.cost, cost)
 
-    def test_markup_gql(self):
-        markup = ItemType.markup(self.item)
+    async def test_markup_gql(self):
+        markup = await ItemType.markup(self.item)
         self.assertEqual(self.item.current_detail.markup, markup)
 
-    def test_creation_date_gql(self):
-        date = ItemType.creation_date(self.item)
+    async def test_creation_date_gql(self):
+        date = await ItemType.creation_date(self.item)
         self.assertEqual(self.item.current_detail.date, date)
 
-    def test_price_gql(self):
-        # Positive Markup
-        price = ItemType.price(self.item)
+    async def test_price_gql(self):
+        item = self.item
+        price = await ItemType.price(item)
         self.assertEqual(15, price)
 
-        # Negative Markup
-        self.item.current_detail.markup = -50
-        price = ItemType.price(self.item)
-        self.assertEqual(5, price)
-
-    def test_version_id(self):
-        id: GlobalID = ItemType.version_id(self.item)
+    async def test_version_id(self):
+        id: GlobalID = await ItemType.version_id(self.item)
         self.assertEqual(f"{self.item.current_detail.id}", f"{id.node_id}")
 
-    def test_item_versions(self):
+    async def test_item_versions(self):
         # Creating a new ItemVersion
-        ItemDetail.objects.create(
+        await sync_to_async(ItemDetail.objects.create)(
             name="itemDetail2",
             barcode="89043",
             cost=10,
@@ -57,8 +52,11 @@ class ItemTypeTest(TestCase):
             root_item=self.item,
         )
 
+        list_coroutine = sync_to_async(list)
         # It is callable but pycharm does not know it
         # noinspection PyCallingNonCallable
-        item_type_result_1 = set(ItemType.item_versions(root=self.item))
-        expected_result = set(ItemDetail.objects.filter(root_item=self.item))
+        temp = await list_coroutine(ItemType.item_versions(root=self.item))
+        item_type_result_1 = set(temp)
+        filtered = await list_coroutine(ItemDetail.objects.filter(root_item=self.item))
+        expected_result = set(filtered)
         self.assertEqual(item_type_result_1, expected_result)
