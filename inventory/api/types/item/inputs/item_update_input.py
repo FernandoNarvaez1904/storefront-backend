9b11c3318ee70b9c7d2c4b8f58f6d721c65f223b
Dery Galeas <dery.reyna@gmail.com>
2022-08-25 05:09:53 -0600

    Item_Update_Input  add errors (NamenotUnique. SKUNotUnique and BarcodeNotUnique)

--- inventory/api/types/item/inputs/item_update_input.py
+++ inventory/api/types/item/inputs/item_update_input.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from asgiref.sync import sync_to_async
 from strawberry_django_plus import gql
 
-from inventory.api.types.item import ItemIsNotActiveError, ItemNotExistError
+from inventory.api.types.item import ItemIsNotActiveError, ItemNotExistError, NameNotUniqueError, BarcodeNotUniqueError, SKUNotUniqueError
 from inventory.models import Item, ItemDetail
 from storefront_backend.api.types import UserError, InputTypeInterface
 
@@ -19,7 +19,9 @@ class ItemUpdateDataInput:
 @gql.django.input(Item)
 class ItemUpdateInput(InputTypeInterface):
     id: gql.relay.GlobalID = gql.field(description="The id given must be of an existing and active Item.")
+    sku: Optional[str]
     data: ItemUpdateDataInput
+    
 
     async def validate_and_get_errors(self) -> List[UserError]:
         errors = []
@@ -33,9 +35,32 @@ class ItemUpdateInput(InputTypeInterface):
                 errors.append(ItemIsNotActiveError(
                     message=f"Item with id {self.id} is not active. Cannot deactivate inactive items"
                 ))
+            
+            
         else:
             errors.append(
-                ItemNotExistError(message=f"item with id {self.id} does not exist in database")
+                ItemNotExistError(message=f"Item with id {self.id} does not exist in database")
             )
+        
+        if input_data := self.data:
+            
+            if input_data.name:
+                if await sync_to_async(ItemDetail.objects.filter(name = self.data.name).exists)():
+                    errors.append(
+                        NameNotUniqueError(message=f"Name is not Unique")
+                    )
 
+            if input_data.barcode:
+                if await sync_to_async(ItemDetail.objects.filter(barcode=self.data.barcode).exists)():
+                    errors.append(
+                        BarcodeNotUniqueError(message=f"Barcode is not unique")
+                    )
+
+        if self.sku:
+                if await sync_to_async(Item.objects.filter(sku = self.sku).exists)():
+                    errors.append(
+                        SKUNotUniqueError(message = f"Sku is not unique")
+                
+        )
+            
         return errors
