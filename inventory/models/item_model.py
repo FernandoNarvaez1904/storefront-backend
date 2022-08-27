c98ff3a97deed64d70cc750050e2fbe4b0edeef9
Diego Romero <diego.romero@unleashtechnologies.net>
2022-08-27 14:20:21 -0600

    Added Flattening

--- inventory/models/item_model.py
+++ inventory/models/item_model.py
@@ -9,12 +9,27 @@ class ItemGroup(models.Model):
 
 
 class Item(models.Model):
+    name = models.CharField(max_length=255, default="")
+    barcode = models.CharField(max_length=48, default="")
+    creation_date = models.DateTimeField(auto_now_add=True, null=True)
+    cost = models.FloatField(default=0)
+    markup = models.FloatField(default=0)
+
     is_active = models.BooleanField(default=True)
     is_service = models.BooleanField(default=False)
     sku = models.CharField(max_length=255, unique=True)
     current_stock = models.FloatField(default=0)
     group = models.ForeignKey(ItemGroup, on_delete=models.SET_NULL, null=True)
     current_detail = models.ForeignKey("ItemDetail", on_delete=models.SET_NULL, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.current_detail:
+            self.name = self.current_detail.name
+            self.barcode = self.current_detail.barcode
+            self.cost = self.current_detail.cost
+            self.markup = self.current_detail.markup
+            self.creation_date = self.current_detail.date
+        super(Item, self).save(*args, **kwargs)
 
 
 class ItemDetail(models.Model):
