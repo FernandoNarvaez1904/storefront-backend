688f17eaa4febb36e20a3b61f3a0b3b313a9d454
Diego Romero <diego.romero@unleashtechnologies.net>
2022-12-01 10:00:40 -0600

    Fix login mutation errors not exported

--- users/api/types/exported_types.py
+++ users/api/types/exported_types.py
@@ -1,7 +1,12 @@
 from users.api.mutations.user_create.user_create_errors import CannotCreateUserUsernameIsNotUnique, \
     CannotCreateUserEmailIsNotUnique
+from users.api.mutations.user_login.user_login_errors import CannotLoginPasswordIsNotCorrect, \
+    CannotLoginUsernameDoesNotExist
 
 exported_types = [
     CannotCreateUserUsernameIsNotUnique,
-    CannotCreateUserEmailIsNotUnique
+    CannotCreateUserEmailIsNotUnique,
+
+    CannotLoginPasswordIsNotCorrect,
+    CannotLoginUsernameDoesNotExist
 ]
