5a7602b8b4b45d1c62536c1caea8b09a7358273c
Diego Romero <diego.romero@unleashtechnologies.net>
2022-12-14 02:24:57 -0600

    Typing test_user_typ

--- users/tests/api/types/test_user_type.py
+++ users/tests/api/types/test_user_type.py
@@ -1,7 +1,9 @@
-import datetime
+from datetime import datetime
+from typing import TypedDict
 
 from asgiref.sync import sync_to_async
 from django.test import TestCase
+from strawberry import ID
 from strawberry_django.utils import is_strawberry_type
 
 from storefront_backend.api.relay.node import Node
@@ -9,69 +11,82 @@ from users.api.types.user_type import UserType
 from users.models import User
 
 
+class DefaultValuesType(TypedDict):
+    id: ID
+    username: str
+    first_name: str
+    last_name: str
+    email: str
+    is_superuser: bool
+    is_staff: bool
+    last_login: datetime
+    date_joined: datetime
+    is_active: bool
+
+
 class TestUserType(TestCase):
-    def setUp(self):
-        self.default_values = {
-            "id": "1",
+    def setUp(self) -> None:
+        self.default_values: DefaultValuesType = {
+            "id": UserType.encode_id("UserType", "1"),
             "username": "username",
             "first_name": "FirstName",
             "last_name": "LastName",
             "email": "email@email.com",
             "is_superuser": False,
             "is_staff": True,
-            "last_login": datetime.datetime.now(),
-            "date_joined": datetime.datetime.now(),
+            "last_login": datetime.now(),
+            "date_joined": datetime.now(),
             "is_active": True
         }
 
-    async def test_is_gql_type(self):
+    async def test_is_gql_type(self) -> None:
         self.assertTrue(is_strawberry_type(UserType))
 
-    async def test_is_node(self):
+    async def test_is_node(self) -> None:
         self.assertTrue(issubclass(UserType, Node))
 
-    async def test_username(self):
-        expected: str = self.default_values.get("username")
+    async def test_username(self) -> None:
+        expected: str = self.default_values["username"]
         user_type_username = UserType(**self.default_values).username
         self.assertEqual(user_type_username, expected)
 
-    async def test_first_name(self):
-        expected: str = self.default_values.get("first_name")
+    async def test_first_name(self) -> None:
+        expected: str = self.default_values["first_name"]
         user_type_first_name = UserType(**self.default_values).first_name
         self.assertEqual(user_type_first_name, expected)
 
-    async def test_last_name(self):
-        expected: str = self.default_values.get("last_name")
+    async def test_last_name(self) -> None:
+        expected: str = self.default_values["last_name"]
         user_type_last_name = UserType(**self.default_values).last_name
         self.assertEqual(user_type_last_name, expected)
 
-    async def test_email(self):
-        expected: str = self.default_values.get("email")
+    async def test_email(self) -> None:
+        expected: str = self.default_values["email"]
         user_type_email = UserType(**self.default_values).email
         self.assertEqual(user_type_email, expected)
 
-    async def test_is_superuser(self):
-        expected: str = self.default_values.get("is_superuser")
+    async def test_is_superuser(self) -> None:
+        expected: bool = self.default_values["is_superuser"]
         user_type_is_superuser = UserType(**self.default_values).is_superuser
         self.assertEqual(user_type_is_superuser, expected)
 
-    async def test_is_staff(self):
-        expected: str = self.default_values.get("is_staff")
+    async def test_is_staff(self) -> None:
+        expected: bool = self.default_values["is_staff"]
         user_type_is_staff = UserType(**self.default_values).is_staff
         self.assertEqual(user_type_is_staff, expected)
 
-    async def test_last_login(self):
-        expected: str = self.default_values.get("last_login")
+    async def test_last_login(self) -> None:
+        expected: datetime = self.default_values["last_login"]
         user_type_last_login = UserType(**self.default_values).last_login
         self.assertEqual(user_type_last_login, expected)
 
-    async def test_date_joined(self):
-        expected: str = self.default_values.get("date_joined")
+    async def test_date_joined(self) -> None:
+        expected: datetime = self.default_values["date_joined"]
         user_type_date_joined = UserType(**self.default_values).date_joined
         self.assertEqual(user_type_date_joined, expected)
 
-    async def test_is_active(self):
-        expected: str = self.default_values.get("is_active")
+    async def test_is_active(self) -> None:
+        expected: bool = self.default_values["is_active"]
         user_type_is_active = UserType(**self.default_values).is_active
         self.assertEqual(user_type_is_active, expected)
 
