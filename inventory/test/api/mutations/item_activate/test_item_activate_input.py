a428fe2f2d5f09bddaaffafd6f3c974bdcc108aa
Diego Romero <diego.romero@unleashtechnologies.net>
2022-12-14 20:02:49 -0600

    Typing test_item_activate_input.py and refactoring

--- inventory/test/api/mutations/item_activate/test_item_activate_input.py
+++ inventory/test/api/mutations/item_activate/test_item_activate_input.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from asgiref.sync import async_to_sync
 from django.test import TestCase
 
 from inventory.api.mutations.item_activate.item_activate_errors import CannotActivateAlreadyActiveItem, \
@@ -12,26 +11,24 @@ from storefront_backend.api.types import UserError
 
 
 class ItemActivateInputTest(TestCase):
-    def setUp(self):
-        items = async_to_sync(create_bulk_of_item)(1)
-        self.item = items[0]
 
-        inactive_items = async_to_sync(create_bulk_of_item)(1, active=False, seed="not")
-        self.inactive_item = inactive_items[0]
-
-    async def test_validate_and_get_errors(self):
+    async def test_validate_and_get_errors_not_existing_id(self) -> None:
         # Test not existing id
         not_existing_item_type = ItemActivateInput(id=Node.encode_id(type_name='ItemType', node_id='3549'))
         expected_not_exist_error: List[UserError] = await not_existing_item_type.validate_and_get_errors()
         self.assertIsInstance(expected_not_exist_error[0], CannotActivateNonExistentItem)
 
+    async def test_validate_and_get_errors_active_item(self) -> None:
         # Test already activate item
+        item = await create_bulk_of_item(1)
         inactive_item_type = ItemActivateInput(
-            id=Node.encode_id(type_name='ItemType', node_id=f"{self.item.id}"))
+            id=Node.encode_id(type_name='ItemType', node_id=f"{item[0].id}"))
         expected_is_not_active_error: List[UserError] = await inactive_item_type.validate_and_get_errors()
         self.assertIsInstance(expected_is_not_active_error[0], CannotActivateAlreadyActiveItem)
 
+    async def test_validate_and_get_errors_no_errors(self) -> None:
         # Test no errors
-        item_type = ItemActivateInput(id=Node.encode_id(type_name='ItemType', node_id=f"{self.inactive_item.id}"))
+        inactive_item = await create_bulk_of_item(1, active=False, seed="not")
+        item_type = ItemActivateInput(id=Node.encode_id(type_name='ItemType', node_id=f"{inactive_item[0].id}"))
         expected_no_error: List[UserError] = await item_type.validate_and_get_errors()
         self.assertFalse(len(expected_no_error))
