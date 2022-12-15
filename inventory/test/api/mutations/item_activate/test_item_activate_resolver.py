0a3b95afd93a3c16ecda026f4de1ebe64f044ae8
Diego Romero <diego.romero@unleashtechnologies.net>
2022-12-14 20:31:25 -0600

    Typing test_item_activate_resolver.py and item_activate_resolver.py and refactoring

--- inventory/test/api/mutations/item_activate/test_item_activate_resolver.py
+++ inventory/test/api/mutations/item_activate/test_item_activate_resolver.py
@@ -40,7 +40,7 @@ class ItemActivateResolverTest(TransactionTestCase):
             "id": async_to_sync(ItemType.get_id_from_model_instance)(self.item),
         }}
 
-    async def test_item_activate_resolver_response(self):
+    async def test_item_activate_resolver_response(self) -> None:
         id_node = Node.encode_id("ItemType", f"{self.item.id}")
         item_input = ItemActivateInput(id=id_node)
         result: ItemActivatePayload = await item_activate_resolver(item_input)
@@ -52,15 +52,17 @@ class ItemActivateResolverTest(TransactionTestCase):
         self.assertFalse(result.user_errors)
 
         # Test if id is not null
-        self.assertIsNotNone(result.node.id)
+        self.assertIsNotNone(result.node)
+        if result.node:
+            self.assertIsNotNone(result.node.id)
 
-        # Test if item is the same that input
-        self.assertEqual(result.node.id, id_node)
+            # Test if item is the same that input
+            self.assertEqual(result.node.id, id_node)
 
-        # Test if item is not is_active
-        self.assertTrue(result.node.is_active)
+            # Test if item is not is_active
+            self.assertTrue(result.node.is_active)
 
-    async def test_item_activate_resolver_side_effect(self):
+    async def test_item_activate_resolver_side_effect(self) -> None:
         # Building input
         id_node = Node.encode_id("ItemType", f"{self.item.id}")
         item_input = ItemActivateInput(id=id_node)
