6c048276fe0ed4d32bf61340a51fac408637427e
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-11 17:42:12 -0600

    create mutation for removing role from user
    ##8669qdz4t[CLOSED]

---
+++ users/api/mutations/role_remove_users/role_remove_users_payload.py
@@ -0,0 +1,13 @@
+from typing import Optional, List
+
+import strawberry
+
+from storefront_backend.api.payload_interface import PayloadTypeInterface
+from storefront_backend.api.types import UserError
+from users.api.types.role_type import RoleType
+
+
+@strawberry.type
+class RoleRemoveUsersPayload(PayloadTypeInterface):
+    node: Optional[RoleType]
+    user_errors: List[UserError]
