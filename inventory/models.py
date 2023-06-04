45fcf18a5d7cb7acbd97e10aaf5e5d98b6e0a479
Diego Romero <diego.romero@unleashtechnologies.net>
2023-06-04 15:13:00 -0600

    Inventory Models

---
+++ inventory/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class ItemCategory(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey("ItemCategory", on_delete=models.PROTECT)
+
+
+# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    cost = models.FloatField()
+    markup = models.FloatField()
+    price_c = models.FloatField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    is_service = models.BooleanField(default=False)
+    unit_of_measure = models.CharField(max_length=255)
+    category = models.ForeignKey(ItemCategory, on_delete=models.PROTECT)
+
+
+class Barcode(models.Model):
+    barcode = models.CharField(max_length=255, unique=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="barcodes")
