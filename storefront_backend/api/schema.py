773cab64ef9d34916c77605e7ed5f411197bb0bb
Diego Romero <diego.romero@unleashtechnologies.net>
2023-06-05 12:36:01 -0600

    Add performance extensions in schema

--- storefront_backend/api/schema.py
+++ storefront_backend/api/schema.py
@@ -1,6 +1,8 @@
 from strawberry import Schema
+from strawberry.extensions import ValidationCache, ParserCache
+from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 
 from storefront_backend.api.mutation import Mutation
 from storefront_backend.api.query import Query
 
-schema = Schema(query=Query, mutation=Mutation)
+schema = Schema(query=Query, mutation=Mutation, extensions=[DjangoOptimizerExtension, ValidationCache(), ParserCache()])
