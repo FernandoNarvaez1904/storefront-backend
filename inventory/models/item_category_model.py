23d566237f46dfaa9408b991ac6f8876dd6f3338
Diego Romero <diego.romero@unleashtechnologies.net>
2023-06-05 15:22:36 -0600

    Making parent in ItemCategory Optional

--- inventory/models/item_category_model.py
+++ inventory/models/item_category_model.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class ItemCategory(models.Model):
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey("ItemCategory", on_delete=models.PROTECT)
+    parent = models.ForeignKey("ItemCategory", on_delete=models.PROTECT, null=True, blank=True)
