0c38f9ecdc0da83609aaf321b51a4b61aeba4257
Diego Romero <diego.romero@unleashtechnologies.net>
2023-06-11 21:08:29 -0400

    Update PaymentMethod Mutation

--- company/api/mutation.py
+++ company/api/mutation.py
@@ -1,9 +1,11 @@
 from strawberry_django_plus import gql
 
-from company.api.types.paymeny_method_type.payment_method_input import PaymentMethodCreateInput
+from company.api.types.paymeny_method_type.payment_method_input import PaymentMethodCreateInput, \
+    PaymentMethodUpdateInput
 from company.api.types.paymeny_method_type.payment_method_type import PaymentMethodType
 
 
 @gql.type
 class Mutation:
     payment_method_create: PaymentMethodType = gql.django.create_mutation(PaymentMethodCreateInput)
+    payment_method_update: PaymentMethodType = gql.django.update_mutation(PaymentMethodUpdateInput)
--- company/api/types/paymeny_method_type/payment_method_input.py
+++ company/api/types/paymeny_method_type/payment_method_input.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from strawberry_django_plus import gql
 
 from company.models import PaymentMethod
@@ -6,3 +8,8 @@ from company.models import PaymentMethod
 @gql.django.input(PaymentMethod)
 class PaymentMethodCreateInput:
     name: str
+
+
+@gql.django.input(PaymentMethod)
+class PaymentMethodUpdateInput(gql.NodeInput):
+    name: Optional[str]
