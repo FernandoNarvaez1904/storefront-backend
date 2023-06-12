54f97865234e2b3354e02ac254d5ce8f14f09a40
Diego Romero <diego.romero@unleashtechnologies.net>
2023-06-11 21:16:24 -0400

    Delete PaymentMethod Mutation

--- company/api/mutation.py
+++ company/api/mutation.py
@@ -9,3 +9,5 @@ from company.api.types.paymeny_method_type.payment_method_type import PaymentMet
 class Mutation:
     payment_method_create: PaymentMethodType = gql.django.create_mutation(PaymentMethodCreateInput)
     payment_method_update: PaymentMethodType = gql.django.update_mutation(PaymentMethodUpdateInput)
+    payment_method_delete: PaymentMethodType = gql.django.delete_mutation(gql.NodeInput,
+                                                                          description="It is only possible to delete PaymentMethods that has no Payment referenced")
