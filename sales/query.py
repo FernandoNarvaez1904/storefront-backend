a6e27d4f9cef36357143c31ccce6cf6d4bd07482
Diego Romero <diego.romero@unleashtechnologies.net>
2023-06-14 17:44:34 -0600

    Get single Client

--- sales/query.py
+++ sales/query.py
@@ -4,11 +4,14 @@ from strawberry_django_plus import gql
 
 from sales.queries.client_queries import client_connection
 from sales.queries.sale_document_queries import total_sales_amount
+from sales.types.client_type.client_type import ClientType
 from sales.types.sale_document_type.sale_document_type import SaleDocumentType
 
 
 @gql.type
 class Query:
     client_connection = client_connection
+    client: ClientType = gql.relay.node()
+    
     total_sales_amount = total_sales_amount
     sale_document: Optional[SaleDocumentType] = gql.relay.node()
